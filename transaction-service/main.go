420e699f8699bd8844bfe6b90c834133bcc88238
JaithunMahira <jaithunmahira@gmail.com>
2022-04-10 16:31:45 +0530

    Added Dockerfile for transaction service

--- transaction-service/main.go
+++ transaction-service/main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("CDP Team - 3")
+	router := gin.Default()
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+	router.Run(":3000")
 }
