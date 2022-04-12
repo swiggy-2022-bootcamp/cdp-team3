92f6afa39f4a800e6a23670c926e853b2cd144b7
dhi13man <dhiman.seal@groww.in>
2022-04-12 16:12:44 +0530

    Cart: API routes added to README

--- cart-service/models/cart_item.go
+++ cart-service/models/cart_item.go
@@ -2,8 +2,14 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
+type Cart struct {
+	Id     primitive.ObjectID `json:"id" validate:"required"`      // MongoDB ID of the cart.
+	UserID string             `json:"user_id" validate:"required"` // ID of the user that the cart belongs to.
+	Items  []CartItem         `json:"items" validate:"required"`   // Items in the User's cart
+}
+
 type CartItem struct {
-	Id        primitive.ObjectID `json:"id" validate:"required"` // MongoDB ID of the cart item
+	Id        primitive.ObjectID `json:"id" validate:"required"`         // MongoDB ID of the cart item
 	ProductID string             `json:"product_id" validate:"required"` // ID of the product
-	Quantity  string             `json:"quantity" validate:"required"` // Quantity of the product in the cart.
+	Quantity  string             `json:"quantity" validate:"required"`   // Quantity of the product in the cart.
 }
